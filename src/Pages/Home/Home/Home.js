3cc763cd56ff1bf9194f613de9f8bed2313702d3
SMhamidAzad <hamidazad020@gmail.com>
2022-04-11 16:33:15 +0600

    explore react bootstrap and bootstrap

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,16 +1,16 @@
 import React from 'react';
+import Banner from '../Banner/Banner';
+import Experts from '../Experts/Experts';
 import Services from '../Services/Services';
-import repair1 from './../../../images/repair-1.png'
 
 const Home = () => {
-    const services = [
-        { id: 1, name: 'oil change', price: 100, description: '', img: 'https://i.ibb.co/XpPJxSG/erik-mclean-3u-Hl-GFnz-PDU-unsplash-1.png'},
-        { id: 1, name: 'oil change', price: 100, description: '', img: 'https://i.ibb.co/XpPJxSG/erik-mclean-3u-Hl-GFnz-PDU-unsplash-1.png'}
-    ]
+
     return (
-        <div>
-           <Services></Services>
-        </div>
+        <>
+            <Banner></Banner>
+            <Services></Services>
+            <Experts></Experts>
+        </>
     );
 };
 
