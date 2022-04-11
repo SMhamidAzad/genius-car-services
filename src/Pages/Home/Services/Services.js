2ba6160c7e3735fa18c502c45188a14ef9fe8cbb
SMhamidAzad <hamidazad020@gmail.com>
2022-04-11 19:44:44 +0600

    module-60 completed

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -3,23 +3,25 @@ import Service from '../Service/Service';
 import './Services.css'
 
 const Services = () => {
-    const [services,setServices] = useState([]);
-    useEffect(()=>{
+    const [services, setServices] = useState([]);
+    useEffect(() => {
         fetch('services.json')
-        .then(res => res.json())
-        .then(data => setServices(data));
-    },[])
+            .then(res => res.json())
+            .then(data => setServices(data));
+    }, [])
     return (
-        <div>
-            <h1 className='services-title my-5'>Our Services</h1>
-           <div className='services-container'>
-           {
-                services.map(service => <Service
-                 key={service.id}
-                 service = {service}
-                ></Service>)
-            }
-           </div>
+        <div className='container'>
+            <div className="row">
+                <h1 className='text-center text-primary my-5'>Our Services</h1>
+                <div className='services-container'>
+                    {
+                        services.map(service => <Service
+                            key={service.id}
+                            service={service}
+                        ></Service>)
+                    }
+                </div>
+            </div>
         </div>
     );
 };
--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer>
-            <p><small>Copyright &copy; {new Date().getUTCFullYear()}</small></p>
+        <footer className='text-center mt-5 bg-black p-3'>
+            <p className='text-white mt-1'><small>Syed Md. Abdul Hamid, Copyright &copy; {new Date().getUTCFullYear()}</small></p>
         </footer>
     );
 };
