abc95a43adfa30b5424e60df42796cacaa0be680
SMhamidAzad <hamidazad020@gmail.com>
2022-04-12 22:58:43 +0600

    responsibe nav bar added

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer className='text-center mt-5 bg-black p-3'>
-            <p className='text-white mt-1'><small>Syed Md. Abdul Hamid, Copyright &copy; {new Date().getUTCFullYear()}</small></p>
+        <p className='text-white mt-1'><small>Syed Md. Abdul Hamid, Copyright &copy; {new Date().getUTCFullYear()}</small></p>
         </footer>
     );
 };
