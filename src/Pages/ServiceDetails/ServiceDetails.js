13bd820a9c7ec98bd1807ecd986c95c6cee61489
SMhamidAzad <hamidazad020@gmail.com>
2022-04-13 21:20:51 +0600

    requireauth rediret and explore react firebase hooks

--- src/Pages/ServiceDetails/ServiceDetails.js
+++ src/Pages/ServiceDetails/ServiceDetails.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 const ServiceDetails = () => {
-    const {serviceId}=useParams()
+    const { serviceId } = useParams()
     return (
         <div>
-            <h2>Welcome to our Details Service: {serviceId}</h2>
+            <h2 className='text-center mb-5'>Welcome to our Details Service: {serviceId}</h2>
+            <div className="text-center">
+                <Link to="/checkout">
+                    <button className='btn btn-primary'>Proceed CheckOut</button>
+                </Link>
+            </div>
         </div>
     );
 };
